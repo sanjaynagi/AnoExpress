0a1c2bb3315592a70baf0bfd7091647c55e643f4
Sanjay C Nagi <sanjay.c.nagi@gmail.com>
2023-03-09 20:50:34 +0000

    sort by position and other tidying

--- tests/test_anoexpress.py
+++ tests/test_anoexpress.py
@@ -5,8 +5,8 @@ import numpy as np
 from pandas.testing import assert_frame_equal
 from numpy.testing import assert_allclose
 
-
-
+gene= 'AGAP006227'
+gene_ids = ['AGAP006222', 'AGAP006226', 'AGAP006227']
 
 @pytest.mark.parametrize(    
         "data_type",
@@ -31,12 +31,36 @@ def test_load_irtex_arrays(data_type):
     assert isinstance(df, pd.DataFrame)
     assert not df.empty
 
+@pytest.mark.parametrize("microarray", [True, False])
+def test_data_irtex(microarray):
+
+    data_df = xpress.data(
+        data_type="fcs",
+        analysis="gamb_colu_arab", 
+        gene_id=None, 
+        microarray=microarray
+    )
+    assert data_df is not None
+    assert not data_df.empty
+    assert isinstance(data_df, pd.DataFrame)
+
+
+@pytest.mark.parametrize("analysis", ['gamb_colu', 'gamb_colu_arab', 'gamb_colu_arab_fun'])
+def test_data_analyses(analysis):
+
+    data_df = xpress.data(
+        data_type="pvals",
+        analysis=analysis, 
+        gene_id=None, 
+        microarray=False
+    )
+    assert data_df is not None
+    assert not data_df.empty
+    assert isinstance(data_df, pd.DataFrame)
 
 @pytest.mark.parametrize('data_type', ["fcs", "pvals", "log2counts"])
-@pytest.mark.parametrize("analysis", ['gamb_colu', 'gamb_colu_arab', 'gamb_colu_arab_fun'])
-@pytest.mark.parametrize("microarray", [True, False])
-@pytest.mark.parametrize("gene_id",    [None, 'AGAP006227', ['AGAP006222', 'AGAP006226', 'AGAP006227']])
-def test_data(data_type, analysis, gene_id, microarray):
+@pytest.mark.parametrize("gene_id",    [None, gene, gene_ids])
+def test_data_types_genes(data_type, analysis, gene_id, microarray):
 
     data_df = xpress.data(
         data_type=data_type,
@@ -51,28 +75,13 @@ def test_data(data_type, analysis, gene_id, microarray):
 
 
 
-
-
-
-
-
-
-
 @pytest.mark.parametrize(
     "gene_ids",
-    ['AGAP006227', ['AGAP006222', 'AGAP006226', 'AGAP006227']]
-)
-@pytest.mark.parametrize(
-    "analysis",
-    ['gamb_colu', 'gamb_colu_arab', 'gamb_colu_arab_fun']
+    [gene, gene_ids]
 )
 @pytest.mark.parametrize(
     "microarray",
     [True, False]
-)
-@pytest.mark.parametrize(
-    'sort_by',
-    ['median', 'mean', 'agap']
 )
 def test_plot_gene_expression(gene_ids, analysis, microarray, sort_by):
 
